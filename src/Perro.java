bdda02297de0012c757e66df7d3a02c21644a1ad
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-13 16:30:00 -0500

    Subclase perro

--- src/Perro.java
+++ src/Perro.java
@@ -1,2 +1,18 @@
-public abstract class Perro {
+public class Perro extends Animal {
+    private String raza;
+
+    public Perro(String nombreAnimal, int edadAnimal, String nombreDuenno, String raza) {
+        super(nombreAnimal, edadAnimal, nombreDuenno);
+        this.raza = raza;
+    }
+
+    @Override
+    public double calcularCostoConsulta() {
+        return 45000.0 + (3000.0 * edadAnimal) ;
+    }
+
+    @Override
+    public String ficha() {
+        return super.ficha() + " | Raza: " + raza;
+    }
 }
